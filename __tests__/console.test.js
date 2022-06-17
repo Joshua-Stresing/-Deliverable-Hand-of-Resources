f26ceb1e69d122a9ccebb6fdaca04f60088866aa
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-17 10:32:42 -0700

    started testing and crud dev for consoles

--- __tests__/console.test.js
+++ __tests__/console.test.js
@@ -8,6 +8,14 @@ describe('console routes', () => {
     return setup(pool);
   });
 
+  it('DELETE /consoles/:id should delete a console', async () => {
+    const resp = await request(app).delete('/consoles/1');
+    expect(resp.status).toEqual(200);
+
+    const { body } = await request(app).get('/consoles/1');
+    expect(body).toEqual(null);
+  });
+
   it('/consoles should show the list of consoles', async () => {
     const res = await request(app).get('/consoles');
     expect(res.body.length).toEqual(5);
