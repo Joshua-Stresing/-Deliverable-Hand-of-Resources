e647991a371df9007295497ebe0cceebfeb78825
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-20 19:55:28 -0700

    details is working:chars

--- __tests__/chars.test.js
+++ __tests__/chars.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('company routes', () => {
+describe('char routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -13,6 +13,16 @@ describe('company routes', () => {
     expect(resp.body.length).toEqual(2);
     const cloud = resp.body.find((char) => char.id === '1');
     expect(cloud).toHaveProperty('name', 'Cloud');
+  });
+
+  it('/chars/:id should return chars details', async () => {
+    const resp = await request(app).get('/chars/1');
+    // expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: '1',
+      name: 'Cloud',
+      description: 'Lead character of Final Fantasy 7',
+    });
   });
 
   afterAll(() => {
